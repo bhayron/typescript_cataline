cd1208793cc32c416a785dbea202a73f9f5bdee5
bhayron <bhayronklivilan@gmail.com>
2022-12-01 22:01:58 -0300

    implementando tipagem em classe

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,47 @@
-console.log("Helo Bhayron rsrs");
+interface User {
+    name:string, 
+    city:string,
+    address:{
+        city?:string
+        UF:string
+    }
+}
+
+
+
+function showCity(user: User){
+    return user.address.city?.toUpperCase()
+}
+
+const response = showCity({
+    name:'bhayron',
+    city:'santa fe',
+    address: {
+        city: 'santa fe',
+        UF:'PE',
+    },
+})
+
+interface Veiculo{
+    rodas: number,
+    acelerar: () => void
+    frear?: () => void
+}
+
+interface Moto extends Veiculo{
+    capacete: boolean,
+    empinar: () => void
+}
+
+let bros: Moto
+
+class CriarVeiculo implements Veiculo{
+    rodas: number
+    constructor(rodas: number){
+        this.rodas = rodas
+    }
+    acelerar(){
+        console.log("Qualquer coisa");
+        
+    }
+}
