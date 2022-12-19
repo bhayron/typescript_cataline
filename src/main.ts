d0e8c31f9ad7eb26bd8c07e62578a5bedbe682eb
bhayron <bhayronklivilan@gmail.com>
2022-12-19 09:57:27 -0300

    Adição de novas funcionalidades

--- src/main.ts
+++ src/main.ts
@@ -48,15 +48,21 @@ class CriarVeiculo implements Veiculo{
 }
 
 const d1:any  = new Date()
-const d2:any    = new Date('2022-11-22T00:00:00.000Z');
-const diffInMs   = d1 - d2
+const d2:any    = new Date('2023-01-03T00:00:00.000Z');
+const diffInMs   = d2 - d1
 const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+const difInDays2 = Math.round(diffInDays)
+//console.log(difInDays2);
 
 if (diffInDays>14) {
     //console.log("Maior que 14");    
 }
 
+const newDate3 = new Date('2023-01-03T00:00:00.000Z');
 
+const newDate4 = (newDate3.getTime() < new Date().getTime())
+
+console.log(newDate4);
 
 var numbers = ['a', 'b', 'f'];
 var doubles = numbers.map(function(value,index) {
@@ -70,6 +76,7 @@ for (let i = 0; i < numbers.length; i++) {
   //console.log(doubles) // 38
 
   const hoje = new Date()
+
 //   console.log(hoje);
 //   console.log(hoje.getDate());
 //   console.log(hoje.getDay() );
@@ -82,7 +89,18 @@ for (let i = 0; i < numbers.length; i++) {
 //   console.log(hoje.getTime());
 //   console.log(hoje.toLocaleDateString() );
   
+const dataTime = '2022-12-16T19:58:24.788Z'
 
+const date2 =  hoje.getTime()
+
+const newDate = new Date(+JSON.parse(JSON.stringify(date2)));
+
+const newDate2 = JSON.parse(JSON.stringify(newDate))
+
+
+// console.log(newDate2);
+const query ={action: "activate"}
+//console.log(query.action);
 
 const selectIdNumberClass = {
     "Divisão 1": 1,
@@ -95,5 +113,39 @@ const selectIdNumberClass = {
     divisaoSelecionado: selectIdNumberClass['Divisão 2'],
   }
 
-  console.log(objetoDivisao);
-  
+ // console.log(objetoDivisao);
+
+
+ let idnumberclass = 'null';
+ let item = 'SPOT 1'
+
+ switch (item) {
+     case "Divisão 1":
+       idnumberclass = '1';
+       break;
+     case "Divisão 2":
+       idnumberclass = '2';
+       break;
+     case "Divisão 3":
+       idnumberclass = '3';
+       break;
+     case "Divisão 4":
+       idnumberclass = '3';
+       break;
+ 
+     case "SPOT 1":
+       idnumberclass ='4';
+       break;
+     case "SPOT 2":
+       idnumberclass = '3';
+       break;
+     case "SPOT 3":
+       idnumberclass = '3';
+       break;
+ 
+     default:
+       idnumberclass = '5';
+       break;
+   }
+   //console.log(idnumberclass);
+   
