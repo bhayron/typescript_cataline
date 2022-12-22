4bf777575176d344c523f8fbde7da05e8573b508
bhayron <bhayronklivilan@gmail.com>
2022-12-22 08:39:30 -0300

    alterações

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ const newDate3 = new Date('2023-01-03T00:00:00.000Z');
 
 const newDate4 = (newDate3.getTime() < new Date().getTime())
 
-console.log(newDate4);
+//console.log(newDate4);
 
 var numbers = ['a', 'b', 'f'];
 var doubles = numbers.map(function(value,index) {
@@ -77,25 +77,41 @@ for (let i = 0; i < numbers.length; i++) {
 
   const hoje = new Date()
 
-//   console.log(hoje);
-//   console.log(hoje.getDate());
-//   console.log(hoje.getDay() );
-//   console.log(hoje.getFullYear());
-//   console.log(hoje.getHours() );
-//   console.log(hoje.getMilliseconds());
-//   console.log(hoje.getMilliseconds() );
-//   console.log(hoje.getMinutes());
-//   console.log(hoje.getMonth());
-//   console.log(hoje.getTime());
-//   console.log(hoje.toLocaleDateString() );
+  // console.log(hoje);
+  // console.log(hoje.getDate());
+  // console.log(hoje.getDay() );
+  // console.log(hoje.getFullYear());
+  // console.log(hoje.getHours() );
+  // console.log(hoje.getMilliseconds());
+  // console.log(hoje.getMilliseconds() );
+  // console.log(hoje.getMinutes());
+  // console.log(hoje.getMonth());
+  // console.log(hoje.getTime());
+  // console.log(hoje.toLocaleDateString() );
   
-const dataTime = '2022-12-16T19:58:24.788Z'
 
-const date2 =  hoje.getTime()
 
-const newDate = new Date(+JSON.parse(JSON.stringify(date2)));
+const dataTime = new Date('2022-11-11T13:52:37.000Z')
+const data = new Date('2022-11-11T13:52:37.000Z').toLocaleDateString('pt-BR', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour:'2-digit',
+  minute:'2-digit',
+  second:'2-digit'
+});
+console.log(data);
 
-const newDate2 = JSON.parse(JSON.stringify(newDate))
+//const dataFormatada = moment(dataTime).format('DD/MM/YYYY')
+
+
+let data_americana = "03-01-2023";
+let data_brasileira = data_americana.split('-').reverse().join('-');
+
+let today = new Date(data_brasileira)
+
+//console.log(today)
+
 
 
 // console.log(newDate2);
@@ -148,4 +164,33 @@ const selectIdNumberClass = {
        break;
    }
    //console.log(idnumberclass);
+   
+   const cpf = "048.138.161-93";
+   const cep = '77848-000'
+
+   //retira os caracteres indesejados...
+  const cpfSem = cpf.replace(/[^\d]/g, '');
+
+//  console.log(cpfSem);
+
+
+let actualCampaign = 'null';
+ let status = '1'
+
+ switch (status) {
+     case "1":
+       actualCampaign = '1';
+       break;
+     case "2":
+       actualCampaign = '2';
+       break;
+     case "3":
+       actualCampaign = '3';
+       break;
+     case "4":
+       actualCampaign = '3';
+       break;
+    
+   }
+   console.log(actualCampaign);
    
